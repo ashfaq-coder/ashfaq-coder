ba96df173c74242fd344c2289dc11bfb82f57dd7
ashfaq-zip <71583707+ashfaq-zip@users.noreply.github.com>
2022-07-27 23:43:07 +0530

    Update quickSort.cpp

--- .github/workflows/quickSort.cpp
+++ .github/workflows/quickSort.cpp
@@ -4,31 +4,29 @@ using namespace std;
 
 int partition(int arr[], int s, int e){
     
-int pivot = arr[s];
+    int pivot = arr[s];
 
-int count = 0;
-for(int i=s+1; i<=e; i++){
-    if(arr[i]<=pivot)
-        count++;
-}
-int pivotIndex = s + count;
-swap(arr[pivotIndex],arr[s]);
-
-int i=s, j=e;
-while(i<pivotIndex && j>pivotIndex){
-    while(arr[i]<=pivot){
-        i++;
+    int count = 0;
+    for(int i=s+1; i<=e; i++){
+        if(arr[i]<=pivot)
+            count++;
     }
-    while(arr[j]>pivot){
-        j--;
-    }
-    if(i<pivotIndex && j>pivotIndex){
-        swap(arr[i++],arr[j--]);
-    }
-}
-return pivotIndex;
+    int pivotIndex = s + count;
+    swap(arr[pivotIndex],arr[s]);
 
-
+    int i=s, j=e;
+    while(i<pivotIndex && j>pivotIndex){
+        while(arr[i]<=pivot){
+            i++;
+        }
+        while(arr[j]>pivot){
+            j--;
+        }
+        if(i<pivotIndex && j>pivotIndex){
+            swap(arr[i++],arr[j--]);
+        }
+    }
+    return pivotIndex;
 }
 
 
